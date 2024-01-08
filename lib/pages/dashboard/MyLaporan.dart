489269edb851f5e200f606c8ca9865404328c36b
ferdians1 <111202012494@mhs.dinus.ac.id>
2024-01-08 12:43:22 +0700

    first init github

---
+++ lib/pages/dashboard/MyLaporan.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:lapor_book/components/list_item.dart';
+import 'package:lapor_book/models/akun.dart';
+import 'package:lapor_book/models/laporan.dart';
+
+class AllLaporan extends StatefulWidget {
+  final Akun akun;
+  AllLaporan({super.key, required this.akun});
+
+  @override
+  State<AllLaporan> createState() => _AllLaporanState();
+}
+
+class _AllLaporanState extends State<AllLaporan> {
+  final _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+  List<Laporan> listLaporan = [];
+  void getTransaksi() async {
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot = await _firestore
+          .collection('laporan')
+          .where('uid',
+              isEqualTo: _auth.currentUser!
+                  .uid) // kondisi untuk menccari laporan yang sesuai dengan akun yang telah login
+          .get();
+
+      setState(() {
+        listLaporan.clear();
+        for (var documents in querySnapshot.docs) {
+          List<dynamic>? komentarData = documents.data()['komentar'];
+
+          List<Komentar>? listKomentar = komentarData?.map((map) {
+            return Komentar(
+              nama: map['nama'],
+              isi: map['isi'],
+            );
+          }).toList();
+          listLaporan.add(
+            Laporan(
+              uid: documents.data()['uid'],
+              docId: documents.data()['docId'],
+              judul: documents.data()['judul'],
+              instansi: documents.data()['instansi'],
+              deskripsi: documents.data()['deskripsi'],
+              nama: documents.data()['nama'],
+              status: documents.data()['status'],
+              gambar: documents.data()['gambar'],
+              tanggal: documents['tanggal'].toDate(),
+              maps: documents.data()['maps'],
+              komentar: listKomentar,
+            ),
+          );
+        }
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getTransaksi();
+
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 1 / 1.234,
+            ),
+            itemCount: listLaporan.length,
+            itemBuilder: (context, index) {
+              // return Text("$index ${listLaporan[index].judul}");
+              return ListItem(
+                laporan: listLaporan[index],
+                akun: widget.akun,
+                isLaporanku: true,
+              );
+            }),
+      ),
+    );
+  }
+}
